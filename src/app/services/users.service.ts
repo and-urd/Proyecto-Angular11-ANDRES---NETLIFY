19dac1483ae02ef95e3e62fa30285c098cad2163
andresurda@gmail.com <andresurda@gmail.com>
2021-04-24 13:51:38 +0200

    users.service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -14,11 +14,10 @@ export class UsersService {
 
   login(user: any): Observable<any>{
     //return this.http.post("https://reqres.in/api/login", user);
+        
+    // return this.http.post("https://proyectofinalurda.herokuapp.com/api/usuario-login", user);
     
-    //https://proyectofinalurda.herokuapp.com
-    return this.http.post("https://proyectofinalurda.herokuapp.com/api/usuario-login", user);
-    
-    // return this.http.post("/api/usuario-login", user);
+    return this.http.post("/api/usuario-login", user);
   }
 
   register(user: any): Observable<any>{

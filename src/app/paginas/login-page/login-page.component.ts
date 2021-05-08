d8775f1a14f55eb2436aefccecc8f13996c22376
andresurda@gmail.com <andresurda@gmail.com>
2021-05-08 18:55:06 +0200

    console.log -> muestra token generado

--- src/app/paginas/login-page/login-page.component.ts
+++ src/app/paginas/login-page/login-page.component.ts
@@ -5,72 +5,76 @@ import { User } from 'src/app/models/user.model';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+	selector: 'app-login-page',
+	templateUrl: './login-page.component.html',
+	styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
 
-  // Atributos
-  username: string = '';
-  email: string = '';
-  password: string = '';
-  loginFallido: boolean = false;
-  hide: boolean = true;
-  spinner: boolean = false;
+	// Atributos
+	username: string = '';
+	email: string = '';
+	password: string = '';
+	loginFallido: boolean = false;
+	hide: boolean = true;
+	spinner: boolean = false;
 
-  user: FormGroup = new FormGroup({});
-  loginForm: FormGroup = new FormGroup({});
+	user: FormGroup = new FormGroup({});
+	loginForm: FormGroup = new FormGroup({});
 
-  // Constructor
-  constructor(
-                public usersService: UsersService, 
-                public router: Router,
-                public formBuilder: FormBuilder) { }
+	// Constructor
+	constructor(
+								public usersService: UsersService, 
+								public router: Router,
+								public formBuilder: FormBuilder) { }
 
-  login(){
-    
-    let user: User = new User(this.loginForm.value.username, this.loginForm.value.email, this.loginForm.value.password, '');
-    
+	login(){
+		
+		let user: User = new User(this.loginForm.value.username, this.loginForm.value.email, this.loginForm.value.password, '');
+		
 
-    this.spinner = true;
-    this.usersService.login(user).subscribe(data => {
-    
-      this.usersService.setToken(data.token);
-      this.usersService.setEstaLogeado(true);
+		this.spinner = true;
+		this.usersService.login(user).subscribe(data => {
+		
+			this.usersService.setToken(data.token);
+			this.usersService.setEstaLogeado(true);
+			
 
-      this.router.navigate(['/home']);
-      // console.log(data);
-      this.spinner = false;
-    },
-    error =>{
-      // console.log(error);
-      this.loginFallido= true;
-      this.usersService.setEstaLogeado(false);
-      this.spinner = false;
-    })
+			console.log('El token generado es este:', this.usersService.getToken());
 
 
-    // console.log(this.email);
-    // console.log(this.password);
-    
-    
-  }
+			this.router.navigate(['/home']);
+			// console.log(data);
+			this.spinner = false;
+		},
+		error =>{
+			// console.log(error);
+			this.loginFallido= true;
+			this.usersService.setEstaLogeado(false);
+			this.spinner = false;
+		})
 
-  ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      username: '',
-      email: '',
-      password: ''
-    });
-  }
 
-  ir_registro(){
-    this.router.navigate(['/register']);
-  }
+		// console.log(this.email);
+		// console.log(this.password);
+		
+		
+	}
 
-  ocultar_informacion_registro(){
-    this.loginFallido = false;
-  }
+	ngOnInit(): void {
+		this.loginForm = this.formBuilder.group({
+			username: '',
+			email: '',
+			password: ''
+		});
+	}
+
+	ir_registro(){
+		this.router.navigate(['/register']);
+	}
+
+	ocultar_informacion_registro(){
+		this.loginFallido = false;
+	}
 
 }
